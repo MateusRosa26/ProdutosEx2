e707922079b59b245b01799a887e9b7f7007e70f
MateusRosa22 <mateus9898@gmail.com>
2025-05-29 06:10:34 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("Arroz", 5.50, 100);
+        Produto produto2 = new Produto("Feijao", 7.20, 80);
+
+        System.out.println("Sistema de produtos iniciado!");
+        System.out.println("Produtos cadastrados com sucesso.");
+    }
+} 
---
+++ src/Produto.java
@@ -0,0 +1,11 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
